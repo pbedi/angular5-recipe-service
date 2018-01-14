25c281530205ab7dabdd103249553a0e92af7f17
Philip Bedi <pbedi@philips-imac.home>
2018-01-14 18:55:51 +0000

    recipe - ingredient update

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -12,7 +12,6 @@ export class RecipeDetailComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    
   }
 
 }
--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -1,12 +1,27 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from 'app/recipes/recipe.model';
+import { Ingredient } from 'app/shared/ingredient.model';
 
 @Injectable()
 export class RecipesService {
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is a test', 'https://ichef.bbci.co.uk/food/ic/food_16x9_506/recipes/cornbread_86386_16x9.jpg'),
-    new Recipe('Another Recipe','This is another test', 'https://ichef.bbci.co.uk/food/ic/food_16x9_506/recipes/cornbread_86386_16x9.jpg')
+    new Recipe(
+      'A Test Recipe',
+      'This is a test', 
+      'https://ichef.bbci.co.uk/food/ic/food_16x9_506/recipes/cornbread_86386_16x9.jpg',
+      [
+        new Ingredient('Orange',2),
+        new Ingredient​​('Bread', 1)
+      ]),
+    new Recipe(
+      'Another Recipe',
+      'This is another test', 
+      'https://ichef.bbci.co.uk/food/ic/food_16x9_506/recipes/cornbread_86386_16x9.jpg',
+      [
+        new Ingredient​​('Buns', 2),
+        new Ingredient​​('Apple', 1)
+      ])
   ];
 
   selecteditem = new EventEmitter<Recipe>();
